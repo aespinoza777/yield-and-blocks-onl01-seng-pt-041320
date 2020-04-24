ea183bc37208d2dffc7ce9de6a92f65ce1770a90
Antonio Espinoza <aespinoza777@gmail.com>
2020-04-24 01:06:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
-def hello_t
-
+def hello(array)
+  arr = []
+  i = 0
+  while i < array.length
+    array << yield(array[i])
+    i += 1
+  end
+arr
 end
 
-# call your method here!
-
+hello(["Tim," "Tom," "Jim"]) {|name| "my name is #{name} i an from somewhere!"}
